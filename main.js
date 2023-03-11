56d624ed35c76532c0cf1286e4ad194e4ca33e00
Jiefei Wang <wangjfmh@gmail.com>
2023-03-11 16:04:45 -0500

    add increaseSA and decreaseSA function in main.js and create index.html file

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-var temperatureChange = function(T, Ts, SA, A) {
-  while (T - Ts < 0) {
-    // increase temperature -> decrease airflow -> decrease SA
-    // every second
-    {
-      SA = SA - 10;
-      T = T + 0.1;
-      // print SA and T
-    }
+var temperatureChange = function(currentT, Ts, currentSA) {
+  // let currentT = T;
+  // let currentSA = SA;
+
+  var increaseSA = function() {
+    currentSA = currentSA + 10;
+    currentT = currentT - 0.5;
+    console.log(`SA is increasing: ${currentSA}`)
+    console.log(`current temperature T: ${currentT.toFixed(1)}`);
   }
-  while (T - Ts > 0) {
-    // decrease temperature -> increase airflow -> increase SA
-    // every second
-    {
-      SA = SA + 10;
-      T = T - 0.1;
-      // print SA and T
-    }
+
+  var decreaseSA = function() {
+    currentSA = currentSA - 10;
+    currentT = currentT + 0.5;
+    console.log(`SA is decreasing: ${currentSA}`)
+    console.log(`current temperature T: ${currentT.toFixed(1)}`);
   }
+
+  let intervalID = setInterval(() => {
+    if (currentT > Ts) {
+      increaseSA();
+    } else if (currentT < Ts) {
+      decreaseSA();
+    } else {
+      clearInterval(intervalID);
+      console.log(`current T is ${currentT}, equal to Ts ${Ts}, current SA is ${currentSA}`)
+    }
+  }, 1000)
 }
+
+temperatureChange(35, 32, 400);
+console.log('...')
+// temperatureChange(32, 36, 460);
