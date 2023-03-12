44cba2d0b29c62804bc726ce3a4da140afca6371
Jiefei Wang <wangjfmh@gmail.com>
2023-03-11 23:39:39 -0500

    add style.css file and create the airflow setpoint will be changed by temperature changing effect

--- main.js
+++ main.js
@@ -1,33 +1,52 @@
-var temperatureChange = function(currentT, Ts, currentSA) {
-  // let currentT = T;
-  // let currentSA = SA;
+function temperatureChange() {
+  let currentTContent = document.getElementById('current-temperature-number');
+  let currentSAContent = document.getElementById('airflowSetpoint-number');
+  let setupTContent = document.getElementById('temperature-setup-number');
+  let changeTContent = document.getElementById('temperature-change');
+  let changeTButton = document.getElementById('temperature-change-submit');
+  let Ts;
+
+  let intervalID = setInterval(() => change(), 1000)
+
+  changeTButton.addEventListener('click', () => {
+    const value = changeTContent.value;
+    setupTContent.innerText = `${value} F`;
+    Ts = value;
+    changeTContent.value = '';
+    clearInterval(intervalID);
+    intervalID = setInterval(() => change(), 1000);
+  })
+
+  let currentT = Number(currentTContent.textContent);
+  let currentSA = Number(currentSAContent.textContent);
 
   var increaseSA = function() {
     currentSA = currentSA + 10;
-    currentT = currentT - 0.5;
-    console.log(`SA is increasing: ${currentSA}`)
-    console.log(`current temperature T: ${currentT.toFixed(1)}`);
+    currentT = currentT - 1;
+    currentSAContent.innerText = currentSA;
+    currentTContent.innerText = currentT;
   }
 
   var decreaseSA = function() {
     currentSA = currentSA - 10;
-    currentT = currentT + 0.5;
-    console.log(`SA is decreasing: ${currentSA}`)
-    console.log(`current temperature T: ${currentT.toFixed(1)}`);
+    currentT = currentT + 1;
+    currentSAContent.innerText = currentSA;
+    currentTContent.innerText = currentT;
   }
 
-  let intervalID = setInterval(() => {
-    if (currentT > Ts) {
-      increaseSA();
-    } else if (currentT < Ts) {
-      decreaseSA();
-    } else {
-      clearInterval(intervalID);
-      console.log(`current T is ${currentT}, equal to Ts ${Ts}, current SA is ${currentSA}`)
+  var change = function() {
+    if (Ts) {
+      if (currentT > Ts) {
+        increaseSA();
+      } else if (currentT < Ts) {
+        decreaseSA();
+      } else {
+        clearInterval(intervalID);
+        console.log(`current T is ${currentT}, equal to Ts ${Ts}, current SA is ${currentSA}`)
+      }
     }
-  }, 1000)
+  }
+
 }
 
-temperatureChange(35, 32, 400);
-console.log('...')
-// temperatureChange(32, 36, 460);
+temperatureChange();
